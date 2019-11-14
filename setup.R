56db012e17a70087cfd7661c9df591b0e1949e2b
Marcus Nunes <marcus.nunes@gmail.com>
2019-11-14 07:32:13 -0300

    Packages needed

---
+++ setup.R
@@ -0,0 +1,19 @@
+# short course setup
+
+packages <-  c("tidyverse",
+               "rlang",
+               "purrr",
+               "lubridate",
+               "glue",
+               "knitr",
+               "rmarkdown",
+               "devtools",
+               "usethis",
+               "rstudioapi",
+               "gapminder")
+
+have   <- rownames(installed.packages())
+needed <- setdiff(packages, have)
+
+install.packages(needed, dependencies = TRUE)
+
