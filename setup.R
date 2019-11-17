4fe08c222ce446de77857f0747df3f7f5c16a887
Marcus Nunes <marcus.nunes@gmail.com>
2019-11-17 08:36:14 -0300

    Final version for the dependencies

--- setup.R
+++ setup.R
@@ -16,6 +16,7 @@ packages <-  c("binb",
                "rstudioapi",
                "rticles",
                "tidyverse",
+               "tinytex",
                "usethis")
 
 have   <- rownames(installed.packages())
